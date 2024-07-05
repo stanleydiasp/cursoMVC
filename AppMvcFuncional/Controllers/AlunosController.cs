bdf78026abaff79d25c208a6a4552689131eb477
stanleydiasp <stanleydiaspaulosz@gmail.com>
2024-07-05 17:27:20 -0300

    Teste

---
+++ AppMvcFuncional/Controllers/AlunosController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AppMvcFuncional.Data;
+using AppMvcFuncional.Models;
+
+namespace AppMvcFuncional.Controllers
+{
+    public class AlunosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlunosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+              return _context.Aluno != null ? 
+                          View(await _context.Aluno.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Aluno'  is null.");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Aluno == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao,Ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aluno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Aluno == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNascimento,Email,Avaliacao,Ativo")] Aluno aluno)
+        {
+            if (id != aluno.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aluno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlunoExists(aluno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Aluno == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Aluno == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Aluno'  is null.");
+            }
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno != null)
+            {
+                _context.Aluno.Remove(aluno);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlunoExists(int id)
+        {
+          return (_context.Aluno?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ AppMvcFuncional/Models/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppMvcFuncional.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo  {0} é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = " O campo {0} precisa ter entre {1} e {30} caracteres}")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo  {0} é obrigatório")]
+        [DataType(DataType.DateTime, ErrorMessage = " O campo {0} precisa ter no máximo {1} caracteres}")]
+        [Display(Name = "Data de nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "O campo  {0} é obrigatório")]
+        [StringLength(60, MinimumLength = 2, ErrorMessage = " O campo {0} precisa ter entre {1} caracteres}")]
+        [RegularExpression(@"^([\w-\.]+@([\w\-]+\.)+[\w-] {2,4}$", ErrorMessage = "O campo {0} está em formato inválido")]
+        //Também pode ser feito desse jeito --> [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "O campo  {0} é obrigatório")]
+        [Display(Name = "Confirme o e-mail")]
+        [Compare("Email", ErrorMessage = "Os e-mails informados não são idênticos")]
+        [NotMapped]
+        public string? EmailConfirmacao { get; set; }
+
+
+        [Required(ErrorMessage = "O campo  {0} é obrigatório")]
+        [Range(1, 5, ErrorMessage = "O campo {0} deve estrar entre {1} e {5}")]
+        public int Avaliacao { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
